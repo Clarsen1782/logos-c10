d50aeb4487df0ea51cb2bccd9518cf921f29e039
Christina Larsen <clarsen1782@outlook.com>
2023-10-22 16:15:13 -0700

    Begining Point on Index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Text for Logo? No more than 3 characters.',
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'What color for text?',
+        },
+        {
+            type: 'checkbox',
+            name: 'shape',
+            message: 'Check shape for Logo',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'color2',
+            message: 'What color for shape background?',
+        },
+    ])
+    .then((answers) => {
+        const logoContent = logo(answers);
+        fs.writeFile('logo.svg', logoContent, (err) =>
+        err ? console.log(err) : console.log('Generated logo.svg')
+        );
+    });
